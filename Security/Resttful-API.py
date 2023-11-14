0b7a4733ea2e2e3bafa2084d198011bf7d100319
MR Wood <mohammad.aldosari@students.plymouth.ac.uk>
2023-11-14 20:43:04 +0000

    setting up API functionlaity to call apis in C# conroller

---
+++ Security/Resttful-API.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+url = ""
+content = ""
+apikey = ""
+headers = {
+    'Content-Type': 'application/json',
+    'ApiKey': f'{apikey}' 
+}
+
+def Get_record_by_email(email):
+    pass
+
+def Post_record(data):
+    pass
+
+def Delete_record_by_email(email):
+    pass
+
+def Update_record_by_email(email):
+    pass
