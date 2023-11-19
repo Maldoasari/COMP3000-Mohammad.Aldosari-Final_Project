659d498ee0ef93f3fac31da30915722a36b9fc83
MR Wood <mohammad.aldosari@students.plymouth.ac.uk>
2023-11-19 11:30:23 +0000

    Startting testing api calls from python file

--- Security/Resttful-API.py
+++ Security/Resttful-API.py
@@ -3,16 +3,18 @@ import json
 
 url = ""
 content = ""
-apikey = ""
+apikey = "5HVEIRO8zc8X6XF-b9Ys0kWXmrSUVjZ0RwGVbD9QsSY="
 headers = {
     'Content-Type': 'application/json',
     'ApiKey': f'{apikey}' 
 }
 
 def Get_record_by_email(email):
+    url = f'http://localhost:5108/api/Voices-Do-services/ByEmail/{email}'
     pass
 
 def Post_record(data):
+    
     pass
 
 def Delete_record_by_email(email):
--- Security/VoicesDoServicesProject/Security/Authentication.cs
+++ Security/VoicesDoServicesProject/Security/Authentication.cs
@@ -9,7 +9,7 @@ using System.Linq; // Make sure to include this for Linq methods like FirstOrDef
 public class ApiKeyAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
 {
     private const string ApiKeyHeaderName = "X-Api-Key";
-    private const string ApiKey = "..ccvvc0-9ujhvd54324wsdvkyubv543442xcvybunimin"; // In a real app, get this from a secure place
+    private const string ApiKey = "5HVEIRO8zc8X6XF-b9Ys0kWXmrSUVjZ0RwGVbD9QsSY="; // In a real app, get this from a secure place
 
     public ApiKeyAuthenticationHandler(
         IOptionsMonitor<AuthenticationSchemeOptions> options,
